9d53afd9dc107b631e9a5c5428181f8d942edb87
Daniel Lindorfer <daniellindorfer@Daniels-iMac.local>
2019-08-12 23:43:51 +0200

    commit props changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/person';
 
 class App extends Component {
   render() {
@@ -7,10 +8,9 @@ class App extends Component {
       <div className="App">
        <h1>Servus im a react app 3</h1>
        <p>Test Paragraph</p>
-       <p>Third paragraph Paragraph</p>
-       <p>Third paragraph Paragraph</p>
-       <p>Second branch</p>
-       <p>third</p>
+       <Person name="MaX" age="28" />
+       <Person name="Tes" age="13">My Hobbies: Racing</Person>
+       <Person name="Ass" age="12" />
       </div>
     );
   }
